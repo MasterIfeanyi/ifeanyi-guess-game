06d17964cd2dd00d24736ae1347e03c024082ae5
IFEANYI <chimaifeanyi29@gmail.com>
2024-04-18 10:51:26 +0100

    add components

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+      <header className="Header">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 text-center">
+              <h3>Guess the Number</h3>
+            </div>
+          </div>
+        </div>
+      </header>
+    )
+}
+  
+export default Header
