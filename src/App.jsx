7127fc0436e2882b54a1e5e7e73c9e1359002de3
IFEANYI <chimaifeanyi29@gmail.com>
2024-04-18 10:58:31 +0100

    add css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Game from './components/Game'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
 
   return (
     <main className="App">
-      <section className="section"> 
-        
-      </section>
+      <Header />
+      <Game />
+      <Footer />
     </main>
   )
 }
